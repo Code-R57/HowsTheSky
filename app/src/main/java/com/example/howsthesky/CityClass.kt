cb3a7d9bf5bbeb9558a707503e6eb4846f07aa60
Code-R57 <codingr57@gmail.com>
2021-08-13 18:52:36 +0530

    UI Changes

--- app/src/main/java/com/example/howsthesky/CityClass.kt
+++ app/src/main/java/com/example/howsthesky/CityClass.kt
@@ -1,6 +1,6 @@
 package com.example.howsthesky
 
 class CityClass(
-    var city: String = "Hyderabad", var temperature: String = "25 °C",
+    var city: String = "Hyderabad", var temperature: String = "25.0 °C",
     var temp_description: String = "Thunderstorm"
 )
